85d1d12724fb7bf6d0e5bc9a2aa2bfc3f1ef28d4
vitorva <vitor.vazafonso@gmail.com>
2022-12-21 14:27:13 +0100

    user entity

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,6 +2,7 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
+import { User } from "./entities/User";
 export default {
   migrations: {
     path: path.join(__dirname, "./migrations"), // path to the folder with migrations
@@ -9,7 +10,7 @@ export default {
   },
   user: "postgres",
   password: "root",
-  entities: [Post],
+  entities: [Post, User],
   dbName: "vareddit",
   type: "postgresql",
   debug: !__prod__,
