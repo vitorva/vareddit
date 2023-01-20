fbde6969c54f19470f689e2eedf2a9f9d0c69d5e
vitorva <vitor.vazafonso@gmail.com>
2023-01-20 16:18:59 +0100

    setup spring backend

---
+++ vareddit-backend/src/test/java/com/project/varedditbackend/VaredditBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.varedditbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaredditBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
